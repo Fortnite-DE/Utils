35bdc29c11090f0d34494601e0d68df16fb1fd2e
Lucas Hardt <Luc1412.lh@gmail.com>
2022-10-09 01:49:23 +0200

    Add Fortnite API icon link function

--- fndeutils/fortnite.py
+++ fndeutils/fortnite.py
@@ -37,3 +37,9 @@ LAST_SEASON_START = datetime.fromtimestamp(1654387201)
 LAST_ARENA_SEASON_START = datetime.fromtimestamp(1655805600)
 
 EVENT_START = None
+
+FORTNITE_API_ICON_LINK = 'https://fortnite-api.com/assets/img/logo_small_128.png?t='
+
+
+def get_fortnite_api_icon_link():
+    return f'FORTNITE_API_ICON_LINK{datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()}'
