eb1b6d2750d0fb90162280ed6f51bc4ccc871fa6
Lucas Hardt <Luc1412.lh@gmail.com>
2023-09-08 02:12:50 +0200

    Implement black and format code

--- fndeutils/view.py
+++ fndeutils/view.py
@@ -5,13 +5,8 @@ from discord.ext import commands
 
 
 class View(discord.ui.View):
-
     async def on_error(
-            self,
-            interaction: discord.Interaction[commands.Bot],
-            error: Exception,
-            item: discord.ui.Item[Any],
-            /
+        self, interaction: discord.Interaction[commands.Bot], error: Exception, item: discord.ui.Item[Any], /
     ) -> None:
         interaction.client.dispatch('error', 'on_view_interaction', self, item, interaction, error=error)
 
@@ -24,14 +19,14 @@ class PaginationView(View):
 
     @classmethod
     async def start(
-            cls,
-            interaction: discord.Interaction,
-            embeds: List[discord.Embed],
-            *,
-            start_page: int = 0,
-            edit: bool = False,
-            ephemeral: bool = False,
-            **kwargs,
+        cls,
+        interaction: discord.Interaction,
+        embeds: List[discord.Embed],
+        *,
+        start_page: int = 0,
+        edit: bool = False,
+        ephemeral: bool = False,
+        **kwargs,
     ):
         if len(embeds) == 0:
             raise ValueError("No embeds provided")
@@ -49,53 +44,33 @@ class PaginationView(View):
             kwargs = dict(embed=embeds[0], ephemeral=ephemeral)
             if view:
                 kwargs["view"] = view
-            respond = (
-                interaction.followup.send
-                if interaction.response.is_done()
-                else interaction.response.send_message
-            )
+            respond = interaction.followup.send if interaction.response.is_done() else interaction.response.send_message
             await respond(**kwargs)
         return view
 
     @discord.ui.button(emoji="⏮️")
-    async def first_page(
-            self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def first_page(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.current_page = 0
         self._update_buttons()
-        await interaction.response.edit_message(
-            embed=self.embeds[self.current_page], view=self
-        )
+        await interaction.response.edit_message(embed=self.embeds[self.current_page], view=self)
 
     @discord.ui.button(emoji="⬅️")
-    async def prev_page(
-            self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def prev_page(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.current_page -= 1
         self._update_buttons()
-        await interaction.response.edit_message(
-            embed=self.embeds[self.current_page], view=self
-        )
+        await interaction.response.edit_message(embed=self.embeds[self.current_page], view=self)
 
     @discord.ui.button(emoji="➡️")
-    async def next_page(
-            self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def next_page(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.current_page += 1
         self._update_buttons()
-        await interaction.response.edit_message(
-            embed=self.embeds[self.current_page], view=self
-        )
+        await interaction.response.edit_message(embed=self.embeds[self.current_page], view=self)
 
     @discord.ui.button(emoji="⏭️")
-    async def last_page(
-            self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
+    async def last_page(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.current_page = len(self.embeds) - 1
         self._update_buttons()
-        await interaction.response.edit_message(
-            embed=self.embeds[self.current_page], view=self
-        )
+        await interaction.response.edit_message(embed=self.embeds[self.current_page], view=self)
 
     def _update_buttons(self):
         if self.current_page == 0:
