7c9230f1ac43f82cd9e5705d41e099d6dc61bf33
Lucas Hardt <Luc1412.lh@gmail.com>
2023-09-10 16:48:53 +0200

    add new util for localization

--- fndeutils/utils.py
+++ fndeutils/utils.py
@@ -1,4 +1,20 @@
+from fortnite_api import GameLanguage
+from redbot.core.i18n import get_locale
+
+
 def unpack_error(e: Exception) -> Exception:
     if hasattr(e, 'original'):
         return unpack_error(e.original)
     return e
+
+
+def get_game_locale() -> GameLanguage:
+    code = get_locale()
+    try:
+        gl = GameLanguage(code)
+    except ValueError:
+        try:
+            gl = GameLanguage(code[:2])
+        except ValueError:
+            gl = GameLanguage.ENGLISH
+    return gl
