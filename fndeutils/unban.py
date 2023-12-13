095352a6ebf6e6cdcfb2f2e59a312773ebe0cf95
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2023-12-13 14:27:58 +0100

    unban system - release

--- fndeutils/unban.py
+++ fndeutils/unban.py
@@ -1,5 +1,5 @@
-TIME_AFTER_BAN = 2 * 60  # 7 * 24 * 60 * 60  # 7 Tage
-TIME_AFTER_TRY = 2 * 60  # 180 * 24 * 60 * 60  # 180 Tage
-VOTE_TIME = 60  # 7 * 24 * 60 * 60  # 7 Tage
-VOTE_OVERTIME = 60  # 7 * 24 * 60 * 60  # 7 Tage
-TIME_ON_UNBAN_SERVER = 300  # 2 * 24 * 60 * 60  # 2 Tage
+TIME_AFTER_BAN = 7 * 24 * 60 * 60  # 7 Tage
+TIME_AFTER_TRY = 180 * 24 * 60 * 60  # 180 Tage
+VOTE_TIME = 7 * 24 * 60 * 60  # 7 Tage
+VOTE_OVERTIME = 14 * 24 * 60 * 60  # 14 Tage
+TIME_ON_UNBAN_SERVER = 2 * 24 * 60 * 60  # 2 Tage
