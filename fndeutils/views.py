40f5feb6aba15a333e4a291b3873d2bd5e012295
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-03-19 13:26:56 +0100

    Update views

--- fndeutils/views.py
+++ fndeutils/views.py
@@ -19,12 +19,13 @@ class View(discord.ui.View):
         timeout: Optional[float] = 300.0,
     ):
         super().__init__(timeout=timeout)
+        self._enabled: bool = interaction is not None
         self.interaction: Optional[discord.Interaction] = interaction
         self.owner: Optional[discord.User] = owner or (interaction.user if interaction else None)
         self.owner_only: bool = owner_only
 
     async def on_timeout(self) -> None:
-        if not self.interaction:
+        if not self.interaction or not self._enabled:
             return
         for item in self.children:
             if item.is_dispatchable():
@@ -32,7 +33,7 @@ class View(discord.ui.View):
         await self.interaction.edit_original_response(view=self)
 
     async def interaction_check(self, interaction: discord.Interaction["Red"], /) -> bool:
-        if self.owner_only and self.interaction and interaction.user != self.owner:
+        if self._enabled and self.owner_only and self.interaction and interaction.user != self.owner:
             embed = discord.Embed(colour=discord.Colour.dark_red())
             embed.description = _('You are not authorized to interact with this menu.')
             await interaction.response.send_message(embed=embed, ephemeral=True)
