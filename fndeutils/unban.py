c106c243313a048a1b1270e75ec677709d50c6db
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-05-11 01:24:24 +0200

    autofill ban reason for valid unban link

--- fndeutils/unban.py
+++ fndeutils/unban.py
@@ -1,4 +1,5 @@
 import discord
+import re
 from typing import Optional
 
 
@@ -9,12 +10,18 @@ VOTE_OVERTIME = 14 * 24 * 60 * 60  # 14 Tage
 TIME_ON_UNBAN_SERVER = 2 * 24 * 60 * 60  # 2 Tage
 
 
-def generate_google_form_link(user: Optional[discord.User] = None) -> str:
+def generate_google_form_link(ban_entry: Optional[discord.BanEntry] = None) -> str:
+    if not ban_entry:
+        return "https://fn-discord.de/entbannung"
+
+    reason = None
+    if ban_entry.reason:
+        reason = re.search(r"Reason: (.+)|Grund: (.+)", ban_entry.reason)
+        if not reason:
+            reason = ban_entry.reason
+
     return (
-        (
-            "https://docs.google.com/forms/d/e/1FAIpQLSf7tmKSzGPJhCtE--QHuQy950BB-siueoR0Wi9DhL9oO4JbTQ/viewform"
-            f"?&entry.1499245679={user}&entry.387590345={user.id}"
-        )
-        if user
-        else "https://fn-discord.de/entbannung"
+        "https://docs.google.com/forms/d/e/1FAIpQLSf7tmKSzGPJhCtE--QHuQy950BB-siueoR0Wi9DhL9oO4JbTQ/viewform"
+        f"?&entry.1499245679={ban_entry.user}&entry.387590345={ban_entry.user.id}"
+        f"{f'&entry.573671189={reason}' if reason else ''}"
 )
