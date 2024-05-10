12b6c6ea8c4067c648c94a123d5c1300d49f4318
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-05-10 19:53:28 +0200

    autofilled form

--- fndeutils/unban.py
+++ fndeutils/unban.py
@@ -8,7 +8,7 @@ VOTE_OVERTIME = 14 * 24 * 60 * 60  # 14 Tage
 TIME_ON_UNBAN_SERVER = 2 * 24 * 60 * 60  # 2 Tage
 
 
-def generate_google_form_link(user: Optional[discord.User] = None):
+def generate_google_form_link(user: Optional[discord.User] = None) -> str:
     return (
         (
             "https://docs.google.com/forms/d/e/1FAIpQLSf7tmKSzGPJhCtE--QHuQy950BB-siueoR0Wi9DhL9oO4JbTQ/viewform"
