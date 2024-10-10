a6361e9bc7f0db35b9e090602323d27c96594266
Jonte Wiemers <jo25bu@gmail.com>
2024-10-10 15:51:49 +0200

    Update unban.py to fix the automatic reason in the google form link

--- fndeutils/unban.py
+++ fndeutils/unban.py
@@ -14,14 +14,26 @@ def generate_google_form_link(ban_entry: Optional[discord.BanEntry] = None) -> s
     if not ban_entry:
         return "https://fn-discord.de/entbannung"
 
-    reason = None
+    reason = ""
     if ban_entry.reason:
-        reason_match = re.search(r"(?:Grund|Reason): (.+)", ban_entry.reason)
-        reason = reason_match.group(1) if reason_match else ban_entry.reason
-        reason = reason.replace(" ", "%20")
+        reason: str = ban_entry.reason
+
+        reason_match = re.search(r"(?:Grund|Reason): (.+)", reason)
+        if reason_match:
+            reason = reason_match.group(1)
+        else:
+            new_search = re.search(r":mod:(.*?):reason:(.*)", reason)
+            if new_search:
+                ### siehe ban_appeals/ban_appeals.py L543
+                # mod = self.server.fn_server.get_member(int(new_search.group(1)))
+                # if mod:
+                #     request.manually_decided = mod
+                reason = new_search.group(2)
+
+    reason = reason.replace(" ", "%20")
 
     return (
         "https://docs.google.com/forms/d/e/1FAIpQLSf7tmKSzGPJhCtE--QHuQy950BB-siueoR0Wi9DhL9oO4JbTQ/viewform"
         f"?&entry.1499245679={ban_entry.user}&entry.387590345={ban_entry.user.id}"
-        f"{f'&entry.573671189={reason}' if reason else ''}"
+        f"{f'&entry.573671189={reason}'}"
     )
