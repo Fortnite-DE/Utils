f27566da4c5a1eee0ff2364d207543fcd645f7ca
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-12-16 15:58:44 +0100

    OG Ranked added

--- fndeutils/matchmaking.py
+++ fndeutils/matchmaking.py
@@ -65,7 +65,7 @@ RANKED_PLAYLIST_TYPES = {
     "ranked-br": 0,
     "ranked-zb": 1,
     "ranked-figment-build": 2,
-    "ranked-figment-nobuild" 3,
+    "ranked-figment-nobuild": 3,
     "ranked_blastberry_build": 4,
     "ranked_blastberry_nobuild": 5,
     "delmar-competitive": 10,
