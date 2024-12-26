8a92d0997a9cd54fba43a9bafceaf50a7a9ad832
Jonte Wiemers <jo25bu@gmail.com>
2024-12-26 01:54:41 +0100

    update fortnite.py

--- fndeutils/fortnite.py
+++ fndeutils/fortnite.py
@@ -1,4 +1,15 @@
 from datetime import datetime, timedelta, timezone
+import discord
+from redbot.core.i18n import Translator
+
+FORTNITE_API_ICON_LINK = 'https://fortnite-api.com/assets/img/logo_small_128.png?t='
+
+def get_fortnite_api_icon_link():
+    return f'{FORTNITE_API_ICON_LINK}{datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()}'
+
+#########################################
+############# Battle Royale #############
+#########################################
 
 SEASON = 33
 SEASONS = {
@@ -28,12 +39,12 @@ SEASONS = {
     24: [4, 2],
     25: [4, 3],
     26: [4, 4],
-    27: [4, 5],
+    27: "Kapitel 1: OG",
     28: [5, 1],
     29: [5, 2],
     30: [5, 3],
     31: [5, 4],
-    32: [5, 5],
+    32: "Kapitel 2: Remix",
     33: [6, 1],
     34: [6, 2],
 }
@@ -44,12 +55,44 @@ SEASON_GRADIENT = [0x8b1ef6, 0x020951]
 SEASON_START = datetime.fromtimestamp(1733022000, tz=timezone.utc)
 SEASON_START_OFFSET = timedelta(hours=6.5)
 SEASON_END = datetime.fromtimestamp(1740132000, tz=timezone.utc)
-LAST_SEASON_START = datetime.fromtimestamp(1730527200, tz=timezone.utc)
 
 EVENT_START = None
 
-FORTNITE_API_ICON_LINK = 'https://fortnite-api.com/assets/img/logo_small_128.png?t='
+#########################################
+############## Fortnite OG ##############
+#########################################
 
+OG_SEASON = 1
+OG_SEASONS = SEASONS
 
-def get_fortnite_api_icon_link():
-    return f'{FORTNITE_API_ICON_LINK}{datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()}'
+OG_SEASON_COVER_URL = 'https://cdn2.unrealengine.com/de-fn33-00-c1s1-discover-playlist-tiles-og-1920x1080-1920x1080-2a20f7eb1c14.jpg?resize=1&w=2560'
+OG_SEASON_COLOR = 0x545a42
+OG_SEASON_GRADIENT = [0xfdf465, 0xf3c516]
+OG_SEASON_START = datetime.fromtimestamp(1733821200, tz=timezone.utc)
+OG_SEASON_START_OFFSET = timedelta(hours=0)
+OG_SEASON_END = datetime.fromtimestamp(1741680000, tz=timezone.utc)
+
+OG_EVENT_START = None
+
+########################################
+############### Festival ###############
+########################################
+
+FESTIVAL_SEASON = 6
+FESTIVAL_ARTISTS = {
+    1: "The Weeknd",
+    2: "Lady Gaga",
+    3: "Billie Eilish",
+    4: "Metallica",
+    5: "Karol G",
+    6: "Snoop Dogg",
+}
+
+FESTIVAL_SEASON_COVER_URL = 'https://cdn2.unrealengine.com/de-fnsp-06-discoverytile-mainstage-1920x1080-1920x1080-9faef70179d3.jpg?resize=1&w=2560'
+FESTIVAL_SEASON_COLOR = 0xf09efe
+FESTIVAL_SEASON_GRADIENT = [0xeadc8c, 0xa77711]
+FESTIVAL_SEASON_START = datetime(2024, 11, 2, 14, tzinfo=timezone.utc)
+FESTIVAL_SEASON_START_OFFSET = timedelta(hours=0)
+FESTIVAL_SEASON_END = datetime(2025, )
+
+FESTIVAL_EVENT_START = None
